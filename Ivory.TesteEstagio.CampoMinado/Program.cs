d5ab9e7c00dc2364beddaa76087cc2248fef5fea
Matheus Fernandes <matheus.fer.0063@gmail.com>
2022-03-22 11:10:28 -0300

    soulucao campo minado Ivory

--- Ivory.TesteEstagio.CampoMinado/Program.cs
+++ Ivory.TesteEstagio.CampoMinado/Program.cs
@@ -11,7 +11,62 @@ namespace Ivory.TesteEstagio.CampoMinado
             Console.WriteLine(campoMinado.Tabuleiro);
 
             // Realize sua codificação a partir deste ponto, boa sorte!
+            Random random = new Random();
 
+            while (campoMinado.JogoStatus == 0)
+            {
+                Console.WriteLine("\nJogo em aberto, continue procurando as posições que não possuem minas terrestres;");
+                var linha = random.Next(1, 10);
+                var coluna = random.Next(1, 10);
+
+                campoMinado.Abrir(linha, coluna);
+                Console.WriteLine("Aperte ENTER para continuar");
+                Console.ReadLine();
+                Console.Clear();
+                Console.WriteLine($"Abrindo CAMPO: Linha({linha})\tColuna({coluna})\n");
+                Console.WriteLine(campoMinado.Tabuleiro);
+            }
+
+            if (campoMinado.JogoStatus == 2)
+            {
+                Console.WriteLine("\nGame Over, você encontrou uma mina terrestre.");
+            }
+            if (campoMinado.JogoStatus == 1)
+            {
+                Console.WriteLine("\nVitoria, você encontrou todas as posições que não possuem minas terrestres.");
+            }
+            Console.ReadKey();
+
+
+
+
+
+            //Usuario escolhendo qual casa abrir
+            /*
+            while (campoMinado.JogoStatus == 0)
+            {
+                Console.WriteLine("\nJogo em aberto, continue procurando as posições que não possuem minas terrestres;");
+                Console.Write("\nQual linha deseja abrir: ");
+                int linha = int.Parse(Console.ReadLine());
+                Console.Write("Qual coluna deseja abrir: ");
+                int coluna = int.Parse(Console.ReadLine());
+
+                campoMinado.Abrir(linha, coluna);
+                Console.Clear();
+                Console.WriteLine(campoMinado.Tabuleiro);
+
+            }
+
+            if (campoMinado.JogoStatus == 2)
+            {
+                Console.WriteLine("\nGame Over, você encontrou uma mina terrestre.");
+            }
+            if (campoMinado.JogoStatus == 1)
+            {
+                Console.WriteLine("\nVitoria, você encontrou todas as posições que não possuem minas terrestres.");
+            }
+            Console.ReadKey();
+            */
         }
     }
 }
